12e85ad7545f6de9adfc56f8f8032feb69aeaea6
Elavarasan8508 <elavarasan723892@gmail.com>
2025-06-12 13:04:29 +0530

    Update deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: '/Mental-health-support-chatbot-with-face-Detection-',
   basePath: '/Mental-health-support-chatbot-with-face-Detection-',
-  trailingSlash: true,
   images: {
     unoptimized: true,
-    domains: ['localhost'],
   },
+  distDir: 'out',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
